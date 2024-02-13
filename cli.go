094baeb066e79eb9a85884b19bd5b75c3ebdbba6
Christian <developer@digitalghost.dev>
2024-02-13 14:12:23 -0800

    rename file, adding welcome message

--- cli.go
+++ cli.go
@@ -1,52 +1,24 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
-	"os"
-
-	"github.com/rocketlaunchr/dataframe-go/imports"
 )
-
-var ctx = context.Background()
 
 func main() {
 	standingsFlag := flag.Bool("standings", false, "Prints current standings")
 	shortStandingsFlag := flag.Bool("s", false, "Prints current standings")
 
 	flag.Usage = func() {
+		fmt.Println("Welcome. This tool simply prints the current standings of the English Premier League.")
 		fmt.Println("Options:")
-		fmt.Println("    -s                    Prints current standings")
-		fmt.Println("    --standings           Prints current standings")
+		fmt.Printf("    %-25s%s\n", "-s, --standings", "Prints current standings")
 	}
 
 	flag.Parse()
 
 	if *standingsFlag || *shortStandingsFlag {
-		fmt.Println("Printing standings...")
-
-		csvFilePath := "standings.csv"
-
-		// Open the CSV file
-		file, err := os.Open(csvFilePath)
-		if err != nil {
-			panic(err)
-		}
-		defer func(file *os.File) {
-			err := file.Close()
-			if err != nil {
-				// Handle the error if needed
-			}
-		}(file)
-
-		// Load the dataframe from the CSV file
-		df, err := imports.LoadFromCSV(ctx, file, imports.CSVLoadOptions{InferDataTypes: true})
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println(df.Table())
+		standings()
 	} else {
 		flag.Usage()
 	}
