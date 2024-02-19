20ea8401a6df933b10838f04c4b86634a16f7d36
Christian <developer@digitalghost.dev>
2024-02-19 12:44:31 -0800

    adding delete methods

--- cli.go
+++ cli.go
@@ -3,15 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/digitalghost-dev/premier-league-cli/cmd"
 )
 
 func main() {
 	standingsFlag := flag.Bool("standings", false, "Prints current standings")
 	shortStandingsFlag := flag.Bool("s", false, "Prints current standings")
 
+	deleteFlag := flag.Bool("delete", false, "Deletes standings.csv file")
+	shortDeleteFlag := flag.Bool("d", false, "Deletes standings.csv file")
+
 	flag.Usage = func() {
 		fmt.Println("Welcome. This tool simply prints the current standings of the English Premier League.")
 		fmt.Println("Options:")
+		fmt.Printf("    %-25s%s\n", "-d, --delete", "Deletes standings.csv file")
 		fmt.Printf("    %-25s%s\n", "-s, --standings", "Prints current standings")
 	}
 
@@ -19,6 +24,8 @@ func main() {
 
 	if *standingsFlag || *shortStandingsFlag {
 		standings()
+	} else if *deleteFlag || *shortDeleteFlag {
+		cmd.DeleteFile("standings.csv")
 	} else {
 		flag.Usage()
 	}
